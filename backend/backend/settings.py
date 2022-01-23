b91952400e5cd3a635b5f857b336f1517a311a36
Juan Muller <juanfra500@gmail.com>
2022-01-23 05:48:11 -0700

    Initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$pet-dthv2z@wi7-*v6ex)p_d97g$uxfz-d-z79wjdhjk1zr=o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
